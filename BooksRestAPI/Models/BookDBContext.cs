eba6c49054278299976b8ca7cb8ddf2e4f45b961
Mehrdad <mehzan07@yahoo.com>
2023-02-16 15:53:06 +0100

    Add project files.

---
+++ BooksRestAPI/Models/BookDBContext.cs
@@ -0,0 +1,16 @@
+﻿
+using BooksRestAPI.Models.Entities;
+using BooksRestAPI.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BooksRestAPI.Models
+{
+    public class BookDBContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public BookDBContext(DbContextOptions<BookDBContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ BooksRestAPI/Models/DataAccess/Contract/IBookRepo.cs
@@ -0,0 +1,15 @@
+﻿using BooksRestAPI.Models.Entities;
+using BooksRestAPI.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BooksRestAPI.Models.DataAccess.Contract
+{
+    public interface IBookRepo
+    {
+        Task<ActionResult<IEnumerable<Book>>> GetBooks();
+        Task<ActionResult<Book>> GetBook(int id);
+        Task<IActionResult> PutBook(int id, Book book);
+        Task<ActionResult<Book>> PostBook(Book book);
+        Task<IActionResult> DeleteBook(int id);
+    }
+}
