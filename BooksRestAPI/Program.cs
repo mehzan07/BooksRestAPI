b6749d4e0b7146020231e67170bac87f28a41319
Mehrdad <mehzan07@yahoo.com>
2023-02-17 16:37:14 +0100

    completting uilder.Services.AddSwaggerGen(c =>

--- BooksRestAPI/Program.cs
+++ BooksRestAPI/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,13 +21,31 @@ builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Product Api",
+        Description = "A simple API to create a product",
+        Contact = new OpenApiContact
+        {
+            Name = "Mehrdad Zandi",
+            Email = "mehrdad.zandi@softsolutionsahand.com",
+            Url = new Uri("http://www.softsolutionsahand.com/")
+        }
+    });
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
 }
 // move these out of if statment to upload Swagger UI
 app.UseSwagger();
